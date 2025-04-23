701e5ce735d328a796a7742ead88aa04e0c2aa82
floarin <florencia.arin@mongodb.com>
2025-04-23 12:28:31 +0200

    recommendation model

--- services/recommendation-ms/app/domain/models/recommendation.py
+++ services/recommendation-ms/app/domain/models/recommendation.py
@@ -1,72 +1,17 @@
-from datetime import datetime
-from typing import List, Dict, Optional
-from bson import ObjectId
-import logging  # <-- Necesario para usar logger
+from dataclasses import dataclass
 
-logger = logging.getLogger(__name__)
-
-class Invoice:
+@dataclass
+class Recommendation:
     """
-    Domain entity representing an invoice.
-    Designed to be framework-agnostic and independent of infrastructure.
+    Domain model representing a recommended product, to be embedded
+    in the user's profile and optionally associated with the invoice.
+
+    This object is typically generated via Vector Search based on
+    product similarity.
     """
-
-    def __init__(
-        self,
-        order_id: str,
-        user_id: str,
-        items: List[Dict],
-        created_at: Optional[datetime] = None,
-        metadata: Optional[Dict] = None,
-        recommendations: Optional[List[Dict]] = None,
-        status: str = "created",
-        _id: Optional[ObjectId] = None
-    ):
-        self._id = _id
-        self.order_id = order_id
-        self.user_id = user_id
-        self.items = items
-        self.created_at = created_at or datetime.utcnow()
-        self.metadata = metadata or {}
-        self.recommendations = recommendations or []
-        self.status = status
-        self.validate()
-
-    def validate(self):
-        if not self.order_id or not self.user_id:
-            raise ValueError("Order ID and User ID are required")
-
-    def calculate_total(self) -> float:
-        return sum(item['price']['amount'] for item in self.items)
-
-    def enrich(self, external_data: Dict):
-        self.metadata.update(external_data)
-
-    def to_dict(self) -> Dict:
-        invoice_dict = {
-            "orderId": self.order_id,
-            "userId": self.user_id,
-            "items": self.items,
-            "createdAt": self.created_at.isoformat(),
-            "metadata": self.metadata,
-            "recommendations": self.recommendations,
-            "status": self.status,
-            "totalAmount": self.calculate_total()
-        }
-
-        if self._id:
-            invoice_dict["_id"] = self._id
-            invoice_dict["invoiceId"] = str(self._id)
-
-        return invoice_dict
-
-    @staticmethod
-    def from_order(order: Dict, _id: Optional[ObjectId] = None):
-        invoice = Invoice(
-            order_id=order["_id"],
-            user_id=order["user"],
-            items=order["products"],
-            _id=_id
-        )
-        logger.info(f"Generated invoice: {invoice.to_dict()}")
-        return invoice
+    product_id: str
+    name: str
+    brand: str
+    price: float
+    image: str
+    vector_search_score: float
--- services/recommendation-ms/app/domain/repositories/recommendation_repository.py
+++ services/recommendation-ms/app/domain/repositories/recommendation_repository.py
@@ -1,40 +1,40 @@
-from app.domain.models.invoice import Invoice
+from app.domain.models.recommendation import Recommendation
 from abc import ABC, abstractmethod
 
-class InvoiceRepository(ABC):
+class RecommendationRepository(ABC):
     """
-    The InvoiceRepository interface defines the methods needed to interact with the persistence layer for invoices.
+    The RecommendationRepository interface defines the methods needed to interact with the persistence layer for recommendations.
     
     The purpose of this interface is to decouple the business logic from the specific infrastructure being used 
     (e.g., MongoDB). This allows for flexibility in changing the database or storage solution without affecting the core application.
     
     Methods:
-        save(invoice: Invoice) -> str: Saves an invoice to the database.
-        find_by_order_id(order_id: str) -> Invoice: Retrieves an invoice by its associated order ID.
+        save(recommendation: Recommendation) -> str: Saves a recommendation to the database.
+        find_by_invoice_id(invoice_id: str) -> Recommendation: Retrieves a recommendation by its associated invoice ID.
     """
     
     @abstractmethod
-    async def save(self, invoice: Invoice) -> str:
+    async def save(self, recommendation: Recommendation) -> str:
         """
-        Save the invoice to the database.
+        Save the recommendation to the database.
 
         Args:
-            invoice (Invoice): The invoice to be saved.
+            recommendation (Recommendation): The recommendation to be saved.
 
         Returns:
-            str: The ID of the saved invoice.
+            str: The ID of the saved recommendation.
         """
         pass
 
     @abstractmethod
-    async def find_by_order_id(self, order_id: str) -> Invoice:
+    async def find_by_invoicer_id(self, invoice_id: str) -> Recommendation:
         """
-        Retrieve an invoice by the associated order ID.
+        Retrieve a recommendation by the associated invoice ID.
 
         Args:
-            order_id (str): The order ID associated with the invoice.
+            invoice_id (str): The invoice ID associated with the recommendation.
         
         Returns:
-            Invoice: The invoice corresponding to the provided order ID.
+            Recommendation: The recommendation corresponding to the provided invoice ID.
         """
         pass
